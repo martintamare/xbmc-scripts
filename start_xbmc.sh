ff52fc1d34b1372b132835ee00b4ceb60944b6b6
Martin Netbox <martin@netbox.(none)>
2011-05-07 16:13:50 +0200

    First commit !

---
+++ start_xbmc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# synchronization of my laptop database and netbox database
+/home/martin/xbmc-scripts/update_macbook.sh
+
+# check if xbmc is launched, if not, proceed to launch
+pid=`pidof xbmc.bin`
+echo $pid
+if [ $pid ]
+then
+	echo "XBMC marche"
+else
+	xbmc
+fi
+
